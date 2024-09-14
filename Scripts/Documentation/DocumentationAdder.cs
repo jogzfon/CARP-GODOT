dfbab29d770fde04d95e2ef0064dd5e0ef73a085
jogzfon <jogzfon@gmail.com>
2024-09-14 23:04:47 +0800

    Document Image Resize Done

--- Scripts/Documentation/DocumentationAdder.cs
+++ Scripts/Documentation/DocumentationAdder.cs
@@ -65,13 +65,29 @@ public partial class DocumentationAdder : Control
             // Create the TextureRect
             var textureRect = new TextureRect
             {
-                Texture = GD.Load<Texture2D>(_selectedImagePath),
-                ExpandMode = TextureRect.ExpandModeEnum.KeepSize,
-                SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter,
-                SizeFlagsVertical = Control.SizeFlags.ShrinkCenter
+                Texture = GD.Load<Texture2D>(_selectedImagePath)
             };
+            var textureSize = textureRect.Texture.GetSize();  // Get the size of the loaded texture
+            if (textureSize.X < 1400)
+            {
+                textureRect.ExpandMode = TextureRect.ExpandModeEnum.KeepSize;
+                textureRect.SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter;
+                textureRect.SizeFlagsVertical = Control.SizeFlags.ShrinkCenter;
+                textureRect.CustomMinimumSize = new Vector2(100, 100);
+            }
+            else
+            {
+                textureRect.ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
+                textureRect.SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter;
+                textureRect.SizeFlagsVertical = Control.SizeFlags.ShrinkCenter;
+                var maxWidth = 1400f;
+                var maxHeight = 1000f;
+                // Calculate scaling factor to maintain aspect ratio within max width and height
+                var scaleFactor = Math.Min(maxWidth / textureSize.X, maxHeight / textureSize.Y);
 
-            textureRect.CustomMinimumSize = new Vector2(100, 100);
+                // Apply the scaling factor to both dimensions to preserve aspect ratio
+                textureRect.CustomMinimumSize = new Vector2(textureSize.X * scaleFactor, textureSize.Y * scaleFactor);
+            }
 
             // Add the TextureRect to the MarginContainer
             marginContainer.AddChild(textureRect);
--- Scripts/Documentation/DocumentationTemplate.cs
+++ Scripts/Documentation/DocumentationTemplate.cs
@@ -96,16 +96,32 @@ public partial class DocumentationTemplate : Control
         marginContainer.AddThemeConstantOverride("margin_bottom", 10);
         marginContainer.AddThemeConstantOverride("margin_left", 10);
         marginContainer.AddThemeConstantOverride("margin_right", 10);
-        TextureRect img = new TextureRect
+        var textureRect = new TextureRect
         {
             Texture = Converter.Base64ToTexture(texture),
-            ExpandMode = TextureRect.ExpandModeEnum.KeepSize,
-            SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter,
-            SizeFlagsVertical = Control.SizeFlags.ShrinkCenter
         };
+        var textureSize = textureRect.Texture.GetSize();  // Get the size of the loaded texture
+        if (textureSize.X < 1400)
+        {
+            textureRect.ExpandMode = TextureRect.ExpandModeEnum.KeepSize;
+            textureRect.SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter;
+            textureRect.SizeFlagsVertical = Control.SizeFlags.ShrinkCenter;
+            textureRect.CustomMinimumSize = new Vector2(100, 100);
+        }
+        else
+        {
+            textureRect.ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
+            textureRect.SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter;
+            textureRect.SizeFlagsVertical = Control.SizeFlags.ShrinkCenter;
+            var maxWidth = 1400f;
+            var maxHeight = 1000f;
+            // Calculate scaling factor to maintain aspect ratio within max width and height
+            var scaleFactor = Math.Min(maxWidth / textureSize.X, maxHeight / textureSize.Y);
 
-        img.CustomMinimumSize = new Vector2(100, 100);
-        marginContainer.AddChild(img);
+            // Apply the scaling factor to both dimensions to preserve aspect ratio
+            textureRect.CustomMinimumSize = new Vector2(textureSize.X * scaleFactor, textureSize.Y * scaleFactor);
+        }
+        marginContainer.AddChild(textureRect);
         _contentContainer.AddChild(marginContainer);
 	}
 }
