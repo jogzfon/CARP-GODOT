d7fd0ad70ef138104b9c23b05d474a4926385ba5
jogzfon <jogzfon@gmail.com>
2024-10-03 02:25:37 +0800

    Removed Unnecessary Classes

--- Scripts/CARPDefault/ALU.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-public class ALU 
-{
-	//Implement the ALU Calculations here.
-}
--- Scripts/CARPDefault/MCodeControlUnit.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-public class MCodeControlUnit
-{
-	//Do MicroCode and HardCode in here
-
-	//Basically make it so you can microprogram and hardwire
-}
